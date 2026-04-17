234db94c731dc83f8fac5dec6559f977c39eb764
ronald <ronaldfoowj65@gmail.com>
2026-04-17 17:21:02 +0800

    todo comment

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ def create_plan(plans):
     planTopic = input('Give this plan a name: ')
     while planTopic == "":
         planTopic = input('It is required to name your plan: ')
+    # [todo] add a while loop to create more plans
     plan = {
         "planId" : str(uuid.uuid4()),
         "planTopic": planTopic,
