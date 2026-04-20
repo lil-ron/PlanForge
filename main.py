cad3a785a997555e1d602f17a5d55b6eb7f9b92d
ronald <ronaldfoowj65@gmail.com>
2026-04-20 17:40:11 +0800

    Append into itineraries array to stack more itineraries

--- main.py
+++ main.py
@@ -8,28 +8,29 @@ def create_plan(plans):
     while planTopic == "":
         planTopic = input('It is required to name your plan: ')
     # [todo] add a while loop to create more plans
+
+    itineraries = []
     while True:
             add_itinerary = input('What is your itinerary for this topic?: ')
             add_itinerary_description = input('What is the description for this itinerary?: ')
-            more_itineraries = input('Do you still want to add more itinerary? (y/n):  ')
-
-            if more_itineraries == 'n':
-                 break
-            
-    plan = {
-        "planId" : str(uuid.uuid4()),
-        "planTopic": planTopic,
-        "itineraries": [
-            {
-                "id": str(uuid.uuid4()),
+            itineraries.append({
+                 "id": str(uuid.uuid4()),
                 "itineraryName": add_itinerary,
                 "itineraryDescription": add_itinerary_description
-            }
-        ],
-        "createdOn": datetime.now().strftime("%Y-%m-%d-%H:%M:%S")
-    }
+            })
 
+            more_itineraries = input('Do you still want to add more itinerary? (y/n):  ')
+            if more_itineraries == 'n':
+                 break
+    plan = {
+        "planId": str(uuid.uuid4()),
+        "planTopic": planTopic,
+        "itineraries": itineraries,
+        "createdOn": datetime.now().strftime("%Y-%m-%d-%H:%M:%S")
+        }
+    
     plans[plan["planId"]] = plan
 
+    
 create_plan(plans)
 print(plans)
